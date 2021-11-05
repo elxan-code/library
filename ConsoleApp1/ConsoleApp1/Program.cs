d6c748b2cf94d11f8770a9dc350b6b4f06b2813d
unknown <you@example.com>
2021-11-05 05:43:53 +0400

    library

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using ikinciproject;
+using System;
+
+namespace Ilkproject
+{
+    class Program : Class1
+    {
+        static void Main(string[] args)
+        {
+           
+            Class1 c = new Class1();
+            c.info();
+            c.INFO();
+            c.BB();
+
+           
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/ikinciprojeckt/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ikinciproject
+{
+    public class Class1
+    {
+        public void info()
+        {
+            Console.WriteLine("sadsadad");
+        }
+        public void aa()
+        {
+            Console.WriteLine("ewrwrwr");
+        }
+
+        
+
+        public void INFO()
+        {
+            Console.WriteLine(" Ifskaflkaf");
+        }
+        public  int BB()
+        {
+            int a, b, c;
+            c = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            a = b + c;
+            return a;
+        }
+
+    }
+} 
